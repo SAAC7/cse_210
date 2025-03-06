a88591b7b1f5fcb96574a69852521e5bc32a81ff
Abraham <saulabraham881@gmail.com>
2025-03-05 23:41:36 -0600

    My first commit before do the excercise

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        hello();
+    }
+    static void hello(){
+        Console.WriteLine("Hello World! This is the Sandbox Project with my modifications");
     }
 }
