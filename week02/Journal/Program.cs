54820015dde0b624379a6bafdccfa76b61f98ba0
Abraham <saulabraham881@gmail.com>
2025-03-12 23:23:46 -0600

    I upload my progress in week 2

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Utilities util = new Utilities();
+        String quetion = util.randomQuestion();
+        Console.WriteLine("" + quetion);
+       
     }
+}
+
+public class Journal{
+
+}
+public class Notes{
+
+}public class Utilities{
+    List<String> questions  = new List<String>{
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
+    public string randomQuestion (){
+        Random random= new Random();
+        int rannumber = random.Next(0, questions.Count-1);
+        return questions[rannumber];
+    }
+
 }
