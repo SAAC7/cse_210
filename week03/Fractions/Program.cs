8998411ef60bb1df1f5ccaa76b9090dd90f8572c
SAAC7 <saulabraham881@gmail.com>
2025-03-18 21:34:44 -0600

    I did the statement of Fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction frac1 = new Fraction();
+        Fraction frac2 = new Fraction(5);
+        Fraction frac3 = new Fraction(3, 4);
+        Fraction frac4 = new Fraction(1, 3);
+
+        List<Fraction> fracarray = new List<Fraction>
+        {
+            frac1, frac2, frac3, frac4
+        };
+
+        foreach (Fraction fr in fracarray)
+        {
+            Console.WriteLine(fr.GetFractionString());
+            Console.WriteLine(fr.GetDecimalValue());
+        }
     }
 }
