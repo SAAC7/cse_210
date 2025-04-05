12b64c5494d4c5d87926f7fb90594cdff0dec0f6
absar_portable <saulabraham881@gmail.com>
2025-04-05 12:39:13 -0600

    I add the comment

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -45,4 +45,16 @@ public class Program
         }
     }
 }
+/*
+Comments on overcoming requirements:
 
+- A system of inheritance and encapsulation has been implemented: the base Activity class defines common attributes and methods
+ (such as StartActivity, EndActivity and pause animation) that are inherited and reused in derived classes.
+- Each activity is in a separate file and the naming convention is followed (TitleCase for classes, 
+ _underscoreCamelCase for member variables, camelCase for local variables).
+- An animation (spinner) has been included that uses backtracking to create the visual effect of pausing.
+- To exceed the requirements, additional functionality can be implemented such as: 
+    - Recording in a log the number of times each activity has been performed.
+    - Control that prompts and questions are not repeated until they have all been shown at least once.
+    - Include more sophisticated text animations (e.g. color or size changes) for the breathing activity.
+*/
